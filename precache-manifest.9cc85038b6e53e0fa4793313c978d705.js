447ce01ba27dc06af427eb5a8e9d4cc9027fbf32
ebrahemsaleh <salehebrahem487@gmail.com>
2021-03-31 07:40:08 +0200

    Updates

---
+++ precache-manifest.9cc85038b6e53e0fa4793313c978d705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54275449ac3cdc7c77ce601f8cc4ef5d",
+    "url": "/Mobile-commerce/index.html"
+  },
+  {
+    "revision": "fdeb95e0dd259ead8a17",
+    "url": "/Mobile-commerce/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "087244ba088657a4407f",
+    "url": "/Mobile-commerce/static/css/main.40401ebf.chunk.css"
+  },
+  {
+    "revision": "fdeb95e0dd259ead8a17",
+    "url": "/Mobile-commerce/static/js/2.fc4d1013.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Mobile-commerce/static/js/2.fc4d1013.chunk.js.LICENSE"
+  },
+  {
+    "revision": "087244ba088657a4407f",
+    "url": "/Mobile-commerce/static/js/main.b720b369.chunk.js"
+  },
+  {
+    "revision": "d545135b82c5be00404b",
+    "url": "/Mobile-commerce/static/js/runtime-main.3a126ee4.js"
+  }
+]);
